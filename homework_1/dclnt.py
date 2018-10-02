72a683639e5c5b19d9e95afa68821aa3ba0b64e2
Dmitry <d.sukhovitsyn@ctm.ru>
2018-10-02 09:23:08 +0300

    Refactoring hometask

--- homework_1/dclnt.py
+++ homework_1/dclnt.py
@@ -1,8 +1,11 @@
 import ast
-import os
 import collections
+import os
+
 
 from nltk import pos_tag
+
+Path = ''
 
 
 def flat(_list):
@@ -14,29 +17,17 @@ def is_verb(word):
     if not word:
         return False
     pos_info = pos_tag([word])
+    print(pos_info)
     return pos_info[0][1] == 'VB'
 
-Path = ''
 
 def get_trees(_path, with_filenames=False, with_file_content=False):
-    filenames = []
     trees = []
-    path= Path
-    for dirname, dirs, files in os.walk(path, topdown=True):
-        for file in files:
-            if file.endswith('.py'):
-                filenames.append(os.path.join(dirname, file))
-                if len(filenames) == 100:
-                    break
+    path = Path
+    filenames = get_py_filenames_in_path(path)
     print('total %s files' % len(filenames))
     for filename in filenames:
-        with open(filename, 'r', encoding='utf-8') as attempt_handler:
-            main_file_content = attempt_handler.read()
-        try:
-            tree = ast.parse(main_file_content)
-        except SyntaxError as e:
-            print(e)
-            tree = None
+        tree = get_file_tree(filename)
         if with_filenames:
             if with_file_content:
                 trees.append((filename, main_file_content, tree))
@@ -57,41 +48,88 @@ def get_verbs_from_function_name(function_name):
 
 
 def get_all_words_in_path(path):
-    trees = [t for t in get_trees(path) if t]
-    function_names = [f for f in flat([get_all_names(t) for t in trees]) if not (f.startswith('__') and f.endswith('__'))]
+    trees = get_filled_trees(path)
+    function_names = [f for f in flat([get_all_names(t) for t in trees])
+                      if not (f.startswith('__') and f.endswith('__'))]
+
     def split_snake_case_name_to_words(name):
         return [n for n in name.split('_') if n]
-    return flat([split_snake_case_name_to_words(function_name) for function_name in function_names])
+
+    return flat([split_snake_case_name_to_words(function_name) for
+                 function_name in function_names])
 
 
 def get_top_verbs_in_path(path, top_size=10):
     global Path
     Path = path
-    trees = [t for t in get_trees(None) if t]
-    fncs = [f for f in flat([[node.name.lower() for node in ast.walk(t) if isinstance(node, ast.FunctionDef)] for t in trees]) if not (f.startswith('__') and f.endswith('__'))]
+    trees = get_filled_trees()
+    fncs = get_public_functions_names_in_trees(trees)
     print('functions extracted')
-    verbs = flat([get_verbs_from_function_name(function_name) for function_name in fncs])
+    verbs = flat([get_verbs_from_function_name(function_name) for
+                  function_name in fncs])
     return collections.Counter(verbs).most_common(top_size)
+
+
 def get_top_functions_names_in_path(path, top_size=10):
-    t = get_trees(path)
-    nms = [f for f in flat([[node.name.lower() for node in ast.walk(t) if isinstance(node, ast.FunctionDef)] for t in t]) if not (f.startswith('__') and f.endswith('__'))]
+    trees = get_filled_trees(path)
+    fncs = get_public_functions_names_in_trees(trees)
     return collections.Counter(nms).most_common(top_size)
 
 
-wds = []
-projects = [
-    'django',
-    'flask',
-    'pyramid',
-    'reddit',
-    'requests',
-    'sqlalchemy',
-]
-for project in projects:
-    path = os.path.join('.', project)
-    wds += get_top_verbs_in_path(path)
+def get_public_functions_names_in_trees(trees):
+    fncs = flat(
+        [
+            [node.name.lower() for node in ast.walk(t)
+             if isinstance(node, ast.FunctionDef)] for t in trees
+         ]
+                )
+    public_fncs = [f for f in fncs
+                   if not (f.startswith('__') and f.endswith('__'))]
+    return public_fncs
 
-top_size = 200
-print('total %s words, %s unique' % (len(wds), len(set(wds))))
-for word, occurence in collections.Counter(wds).most_common(top_size):
-print(word, occurence)
+
+def get_filled_trees(path=None):
+    return [t for t in get_trees(path) if t]
+
+
+def get_py_filenames_in_path(path):
+    filenames = []
+    for dirname, dirs, files in os.walk(path, topdown=True):
+        for file in files:
+            if file.endswith('.py'):
+                filenames.append(os.path.join(dirname, file))
+            if len(filenames) == 100:
+                break
+    return filenames
+
+
+def get_file_tree(filename):
+    with open(filename, 'r', encoding='utf-8') as attempt_handler:
+        main_file_content = attempt_handler.read()
+    try:
+        tree = ast.parse(main_file_content)
+    except SyntaxError as e:
+        print(e)
+        tree = None
+    return tree
+
+
+if __name__ == "__main__":
+    wds = []
+    projects = [
+        'django',
+        'flask',
+        'pyramid',
+        'reddit',
+        'requests',
+        'sqlalchemy',
+    ]
+    for project in projects:
+        path = os.path.join('.', project)
+        print(path)
+        wds += get_top_verbs_in_path(path)
+
+    top_size = 200
+    print('total %s words, %s unique' % (len(wds), len(set(wds))))
+    for word, occurence in collections.Counter(wds).most_common(top_size):
+        print(word, occurence)
