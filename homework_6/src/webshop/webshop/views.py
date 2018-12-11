23df1c0ef064e892cbb71a62df3dec50e7c2e069
Dmitry <d.sukhovitsyn@ctm.ru>
2018-12-11 16:01:27 +0300

    added 500.html , block tittle, starts work with django-configurations

--- homework_6/src/webshop/webshop/views.py
+++ homework_6/src/webshop/webshop/views.py
@@ -10,7 +10,7 @@ def handler404(request, *args, **argv):
 
 
 def handler500(request, *args, **argv):
-    response = render_to_response('404.html', {},
+    response = render_to_response('500.html', {},
                                   context_instance=RequestContext(request))
     response.status_code = 500
     return response
