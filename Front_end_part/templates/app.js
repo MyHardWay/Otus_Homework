3086bd9e8cb02becf28b8e3c7b42bd3f61e7e97c
Dmitry <d.sukhovitsyn@ctm.ru>
2019-01-21 13:55:48 +0300

    added courses page with lessons and js animations

--- Front_end_part/templates/app.js
+++ Front_end_part/templates/app.js
@@ -1,23 +1,37 @@
 function init()  {
     addEv2Cls('card__fio', log, 'click');
     addEv2Cls('header__item', changeCss, 'mouseenter');
+    addEv2Cls('lessons__name', hideAndShow, 'click')
 };
 
 
 function addEv2Cls(cls_name, ev, action_) {
     var classes = document.getElementsByClassName(cls_name);
     Array.from(classes).forEach(function(element) {
-        alert(element);
         element.addEventListener(action_, function(){ev(element);}, false);
     });
 };
 
 
 function log(elem) {
-    alert(elem);
+    console.log(elem);
 };
 
 function changeCss(elem) {
     var child = elem.firstElementChild;
     child.style.color = 'red';
 };
+
+function hideAndShow(elem) {
+    parent = elem.parentNode;
+    var closest = parent.getElementsByClassName('lessons__content')[0];
+    if (closest.style.display == 'none' || closest.style.display == '') {
+        closest.style.display = 'block';
+        } else {
+        closest.style.display = 'none';
+        };
+    };
+
+
+
+
