cd89aa5164d190d441a94d29e78d4df95200db13
root <11adima@mail.ru>
2019-01-22 10:33:00 -0800

    Fixed bugs and trying to do less to css

--- Front_end_part/static/js/login.js
+++ Front_end_part/static/js/login.js
@@ -1,5 +1,13 @@
 export function login (log_, pass_) {
     let login_ = $(log_).val();
-    let password_ = $(pass_).val();
-    console.log('Login: ' +  $(log_).val() + ' Password: ' +  $(pass_).val())
+    let passw_ = $(pass_).val();
+     fetch('http://127.0.0.1:4000/login/', {
+        method: 'post',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          },
+        body: JSON.stringify(
+            {'login': login_, 'password': passw_})})
+            .then(res => console.log(res));
 }
--- Front_end_part/webpack.config.js
+++ Front_end_part/webpack.config.js
@@ -1,5 +1,7 @@
 var path = require('path');
 var webpack = require('webpack');
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 
 module.exports = {
@@ -9,11 +11,33 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
 
+   module: {
+        rules: [
+
+            {
+                test: /\.less$/, // .less and .css
+                use: [
+                       MiniCssExtractPlugin.loader,
+                       "css-loader",
+                       'less-loader'
+                ]
+          }
+        ]
+    },
+
+
   plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery"
-        })
-    ]
-
+        }),
+        new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+        filename: "[name].css",
+        chunkFilename: '[id].css',
+         })
+        ]
 }
+
+
